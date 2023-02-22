d493f663f4238df1c8a0c7fc39ec487758c98459
DELL <sbhujbal023@gmail.com>
2023-02-21 23:20:38 -1000

    search page feature code

---
+++ src/test/java/com/qa/pageboject/Searchpage.java
@@ -0,0 +1,17 @@
+package com.qa.pageboject;
+
+public class Searchpage {
+
+	public void test() {
+	
+	System.out.println("search page test");
+	
+	}
+
+
+public void setlogin() {
+
+System.out.println("set login test");
+
+}
+}
